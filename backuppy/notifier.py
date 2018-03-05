d2b42aab45dbb0828f2122ee9584c9a6c70fc0b4
Bart Feenstra <bart@mynameisbart.com>
2018-03-05 22:33:46 +0100

    When invoked through the CLI, send notifications to stdio.

--- backuppy/notifier.py
+++ backuppy/notifier.py
@@ -1,12 +1,36 @@
 """Provide notifications."""
+from __future__ import print_function
+
 import subprocess
+import sys
 
 
 class Notifier:
     """Define a notifier."""
 
-    def notify(self, message):
-        """Send a notification.
+    def state(self, message):
+        """Send a notification that may be ignored.
+
+        :param message: str
+        """
+        pass
+
+    def inform(self, message):
+        """Send an informative notification.
+
+        :param message: str
+        """
+        pass
+
+    def confirm(self, message):
+        """Send a confirmation/success notification.
+
+        :param message: str
+        """
+        pass
+
+    def alert(self, message):
+        """Send an error notification.
 
         :param message: str
         """
@@ -23,13 +47,37 @@ class GroupedNotifiers(Notifier):
         """
         self._notifiers = notifiers
 
-    def notify(self, message):
-        """Send a notification.
+    def state(self, message):
+        """Send a notification that may be ignored.
 
         :param message: str
         """
         for notifier in self._notifiers:
-            notifier.notify(message)
+            notifier.state(message)
+
+    def inform(self, message):
+        """Send an informative notification.
+
+        :param message: str
+        """
+        for notifier in self._notifiers:
+            notifier.inform(message)
+
+    def confirm(self, message):
+        """Send a confirmation/success notification.
+
+        :param message: str
+        """
+        for notifier in self._notifiers:
+            notifier.confirm(message)
+
+    def alert(self, message):
+        """Send an error notification.
+
+        :param message: str
+        """
+        for notifier in self._notifiers:
+            notifier.alert(message)
 
 
 class NotifySendNotifier(Notifier):
@@ -38,9 +86,93 @@ class NotifySendNotifier(Notifier):
     See https://linux.die.net/man/2/send.
     """
 
-    def notify(self, message):
+    def _notify(self, message):
         """Send a notification.
 
         :param message: str
         """
         subprocess.call(('notify-send', "'%s'" % message))
+
+    def state(self, message):
+        """Send a notification that may be ignored.
+
+        :param message: str
+        """
+        self._notify(message)
+
+    def inform(self, message):
+        """Send an informative notification.
+
+        :param message: str
+        """
+        self._notify(message)
+
+    def confirm(self, message):
+        """Send a confirmation/success notification.
+
+        :param message: str
+        """
+        self._notify(message)
+
+    def alert(self, message):
+        """Send an error notification.
+
+        :param message: str
+        """
+        self._notify(message)
+
+
+class FileNotifier(Notifier):
+    """Send notifications to files."""
+
+    def __init__(self, state_file, inform_file, confirm_file, alert_file):
+        """Initialize a new instance.
+
+        :param state_file: File
+        :param inform_file: File
+        :param confirm_file: File
+        :param alert_file: File
+        """
+        self._state_file = state_file
+        self._inform_file = inform_file
+        self._confirm_file = confirm_file
+        self._alert_file = alert_file
+
+    @classmethod
+    def for_stdio(cls):
+        """Get a new notifier for stdout and stderr.
+
+        :return: cls
+        """
+        return cls(sys.stdout, sys.stdout, sys.stdout, sys.stderr)
+
+    def _print(self, message, color, file):
+        print('\033[0;%dm  \033[0;1;%dm %s\033[0m' % (color + 40, color + 30, message), file=file)
+
+    def state(self, message):
+        """Send a notification that may be ignored.
+
+        :param message: str
+        """
+        self._print(message, 7, self._state_file)
+
+    def inform(self, message):
+        """Send an informative notification.
+
+        :param message: str
+        """
+        self._print(message, 6, self._inform_file)
+
+    def confirm(self, message):
+        """Send a confirmation/success notification.
+
+        :param message: str
+        """
+        self._print(message, 2, self._confirm_file)
+
+    def alert(self, message):
+        """Send an error notification.
+
+        :param message: str
+        """
+        self._print(message, 1, self._alert_file)
--- backuppy/tests/test_notifier.py
+++ backuppy/tests/test_notifier.py
@@ -8,22 +8,76 @@ from backuppy.notifier import NotifySendNotifier, GroupedNotifiers
 
 
 class GroupedNotifiersTest(TestCase):
-    def test_notify(self):
+    def test_state(self):
         notifier_1 = Mock()
         notifier_2 = Mock()
         notifier_3 = Mock()
         sut = GroupedNotifiers([notifier_1, notifier_2, notifier_3])
         message = 'Something happened!'
-        sut.notify(message)
-        notifier_1.notify.assert_called_with(message)
-        notifier_2.notify.assert_called_with(message)
-        notifier_3.notify.assert_called_with(message)
+        sut.state(message)
+        notifier_1.state.assert_called_with(message)
+        notifier_2.state.assert_called_with(message)
+        notifier_3.state.assert_called_with(message)
+
+    def test_inform(self):
+        notifier_1 = Mock()
+        notifier_2 = Mock()
+        notifier_3 = Mock()
+        sut = GroupedNotifiers([notifier_1, notifier_2, notifier_3])
+        message = 'Something happened!'
+        sut.inform(message)
+        notifier_1.inform.assert_called_with(message)
+        notifier_2.inform.assert_called_with(message)
+        notifier_3.inform.assert_called_with(message)
+
+    def test_confirm(self):
+        notifier_1 = Mock()
+        notifier_2 = Mock()
+        notifier_3 = Mock()
+        sut = GroupedNotifiers([notifier_1, notifier_2, notifier_3])
+        message = 'Something happened!'
+        sut.confirm(message)
+        notifier_1.confirm.assert_called_with(message)
+        notifier_2.confirm.assert_called_with(message)
+        notifier_3.confirm.assert_called_with(message)
+
+    def test_alert(self):
+        notifier_1 = Mock()
+        notifier_2 = Mock()
+        notifier_3 = Mock()
+        sut = GroupedNotifiers([notifier_1, notifier_2, notifier_3])
+        message = 'Something happened!'
+        sut.alert(message)
+        notifier_1.alert.assert_called_with(message)
+        notifier_2.alert.assert_called_with(message)
+        notifier_3.alert.assert_called_with(message)
 
 
 class NotifySendNotifierTest(TestCase):
     @patch('subprocess.call')
-    def test_notify(self, m):
+    def test_state(self, m):
         sut = NotifySendNotifier()
         message = 'Something happened!'
-        sut.notify(message)
+        sut.state(message)
+        m.assert_called_with(('notify-send', "'%s'" % message))
+
+    @patch('subprocess.call')
+    def test_inform(self, m):
+        sut = NotifySendNotifier()
+        message = 'Something happened!'
+        sut.inform(message)
+        m.assert_called_with(('notify-send', "'%s'" % message))
+
+    @patch('subprocess.call')
+    def test_confirm(self, m):
+        sut = NotifySendNotifier()
+        message = 'Something happened!'
+        sut.confirm(message)
+        m.assert_called_with(('notify-send', "'%s'" % message))
+
+    @patch('subprocess.call')
+    def test_alert(self, m):
+        sut = NotifySendNotifier()
+        message = 'Something happened!'
+        sut.alert(message)
         m.assert_called_with(('notify-send', "'%s'" % message))
