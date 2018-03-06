cbf24f97e071bb6e049baf28cba9d152463f178c
Bart Feenstra <bart@mynameisbart.com>
2018-03-06 09:19:24 +0100

    Enable dependencies and the project description in setup.py.

--- setup.py
+++ setup.py
@@ -7,18 +7,18 @@ VERSION = '0.0.1'
 
 REQUIREMENTS_PATH = '/'.join((os.path.dirname(os.path.abspath(__file__)), 'requirements.txt'))
 
-# with open(REQUIREMENTS_PATH) as f:
-#     dependencies = f.read().split("\n")
+with open(REQUIREMENTS_PATH) as f:
+    dependencies = f.read().split("\n")
 
 SETUP = {
     'name': "backuppy",
     'description': 'A back-up tool.',
-    # 'long_description': open('README.md').read(),
+    'long_description': open('README.md').read(),
     'version': VERSION,
     'license': "MIT",
     'author': "Bart Feenstra",
     'url': "https://github.com/bartfeenstra/backuppy",
-    # 'install_requires': dependencies,
+    'install_requires': dependencies,
     'packages': find_packages(),
     'scripts': [
         'bin/backuppy',
