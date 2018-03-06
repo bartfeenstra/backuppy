6f18ac92c38906bb1d7c8a839ac628d9b27fe823
Bart Feenstra <bart@mynameisbart.com>
2018-03-06 11:55:56 +0100

    Improve test coverage of the configuration component.

--- backuppy/tests/test_config.py
+++ backuppy/tests/test_config.py
@@ -1,3 +1,5 @@
+import json
+from tempfile import NamedTemporaryFile
 from unittest import TestCase
 
 from backuppy.tests import CONFIGURATION_PATH
@@ -19,6 +21,15 @@ class ConfigurationTest(TestCase):
         self.assertTrue(sut.verbose)
         sut.verbose = False
         self.assertFalse(sut.verbose)
+
+    def test_verbose_non_boolean(self):
+        with open('%s/backuppy.json' % CONFIGURATION_PATH) as f:
+            configuration = json.load(f)
+        configuration['verbose'] = 666
+        with NamedTemporaryFile(mode='w+t') as f:
+            json.dump(configuration, f)
+            with self.assertRaises(ValueError):
+                Configuration.from_configuration_data(f.name, configuration)
 
     def test_working_directory(self):
         configuration_file_path = '%s/backuppy.json' % CONFIGURATION_PATH
@@ -49,6 +60,60 @@ class ConfigurationTest(TestCase):
         notifiers = [Mock(PluginConfiguration), Mock(PluginConfiguration)]
         sut = Configuration(configuration_file_path, source, [target], notifiers=notifiers)
         self.assertEquals(sut.notifiers, notifiers)
+
+    def test_notifier_type_missing(self):
+        with open('%s/backuppy.json' % CONFIGURATION_PATH) as f:
+            configuration = json.load(f)
+        del configuration['notifications'][0]['type']
+        with NamedTemporaryFile(mode='w+t') as f:
+            json.dump(configuration, f)
+            with self.assertRaises(ValueError):
+                Configuration.from_configuration_data(f.name, configuration)
+
+    def test_source_missing(self):
+        with open('%s/backuppy.json' % CONFIGURATION_PATH) as f:
+            configuration = json.load(f)
+        del configuration['source']
+        with NamedTemporaryFile(mode='w+t') as f:
+            json.dump(configuration, f)
+            with self.assertRaises(ValueError):
+                Configuration.from_configuration_data(f.name, configuration)
+
+    def test_source_type_missing(self):
+        with open('%s/backuppy.json' % CONFIGURATION_PATH) as f:
+            configuration = json.load(f)
+        del configuration['source']['type']
+        with NamedTemporaryFile(mode='w+t') as f:
+            json.dump(configuration, f)
+            with self.assertRaises(ValueError):
+                Configuration.from_configuration_data(f.name, configuration)
+
+    def test_targets_missing(self):
+        with open('%s/backuppy.json' % CONFIGURATION_PATH) as f:
+            configuration = json.load(f)
+        del configuration['targets']
+        with NamedTemporaryFile(mode='w+t') as f:
+            json.dump(configuration, f)
+            with self.assertRaises(ValueError):
+                Configuration.from_configuration_data(f.name, configuration)
+
+    def test_target_type_missing(self):
+        with open('%s/backuppy.json' % CONFIGURATION_PATH) as f:
+            configuration = json.load(f)
+        del configuration['targets'][0]['type']
+        with NamedTemporaryFile(mode='w+t') as f:
+            json.dump(configuration, f)
+            with self.assertRaises(ValueError):
+                Configuration.from_configuration_data(f.name, configuration)
+
+    def test_targets_empty(self):
+        with open('%s/backuppy.json' % CONFIGURATION_PATH) as f:
+            configuration = json.load(f)
+        configuration['targets'] = []
+        with NamedTemporaryFile(mode='w+t') as f:
+            json.dump(configuration, f)
+            with self.assertRaises(ValueError):
+                Configuration.from_configuration_data(f.name, configuration)
 
 
 class FromJsonTest(TestCase):
