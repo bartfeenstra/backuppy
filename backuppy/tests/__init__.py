8dfedbc9d62a90d26cd0d85221cc32bb27cdf4d9
Bart Feenstra <bart@mynameisbart.com>
2018-05-28 20:21:48 +0200

    Clean up the path asserts.

--- backuppy/tests/__init__.py
+++ backuppy/tests/__init__.py
@@ -4,3 +4,52 @@ RESOURCE_PATH = '/'.join(
     (os.path.dirname(os.path.abspath(__file__)), 'resources'))
 
 CONFIGURATION_PATH = '/'.join((RESOURCE_PATH, 'configuration'))
+
+
+def assert_paths_identical(test, source_path, target_path):
+    """Assert the source and target directories are identical.
+
+    :param test: unittest.TestCase
+    :param source_path: str
+    :param target_path: str
+    :raise: AssertionError
+    """
+    assert_path_appears(test, source_path, target_path)
+    assert_path_appears(test, target_path, source_path)
+
+
+def assert_path_appears(test, source_path, target_path):
+    """Assert the contents of one directory appear in another.
+
+    :param test: unittest.TestCase
+    :param source_path: str
+    :param target_path: str
+    :raise: AssertionError
+    """
+    source_path = source_path.rstrip('/') + '/'
+    target_path = target_path.rstrip('/') + '/'
+    try:
+        for target_dir_path, child_dir_names, child_file_names in os.walk(target_path):
+            source_dir_path = os.path.join(
+                source_path, target_dir_path[len(target_path):])
+            for child_file_name in child_file_names:
+                with open(os.path.join(target_dir_path, child_file_name)) as target_f:
+                    with open(os.path.join(source_dir_path, child_file_name)) as source_f:
+                        assert_file(test, source_f, target_f)
+    except Exception:
+        raise AssertionError(
+            'The source contents under the path `%s` are not equal to the target contents under `%s`.' % (
+                source_path, target_path))
+
+
+def assert_file(test, source_f, target_f):
+    """Assert two source and target files are identical.
+
+    :param test: unittest.TestCase
+    :param source_f: File
+    :param target_f: File
+    :raise: AssertionError
+    """
+    source_f.seek(0)
+    target_f.seek(0)
+    test.assertEquals(source_f.read(), target_f.read())
--- backuppy/tests/test_task.py
+++ backuppy/tests/test_task.py
@@ -5,9 +5,9 @@ from unittest import TestCase
 
 from parameterized import parameterized
 
-from backuppy import assert_path
 from backuppy.location import PathSource, PathTarget, FilePath, DirectoryPath
 from backuppy.task import backup, restore
+from backuppy.tests import assert_paths_identical
 
 try:
     from unittest.mock import Mock
@@ -53,7 +53,7 @@ class BackupTest(TestCase):
                 # Back up the first time.
                 result = backup(configuration)
                 self.assertTrue(result)
-                assert_path(self, source_path, os.path.join(
+                assert_paths_identical(self, source_path, os.path.join(
                     target_path, 'latest'))
                 real_snapshot_1_path = subprocess.check_output(['readlink', '-f', 'latest'], cwd=target_path).decode(
                     'utf-8').strip()
@@ -64,13 +64,13 @@ class BackupTest(TestCase):
 
                 result = backup(configuration)
                 self.assertTrue(result)
-                assert_path(self, os.path.join(
+                assert_paths_identical(self, os.path.join(
                     target_path, 'latest'), source_path)
                 real_snapshot_2_path = subprocess.check_output(['readlink', '-f', 'latest'], cwd=target_path).decode(
                     'utf-8').strip()
 
                 # Ensure the previous snapshot has not changed.
-                assert_path(self, real_snapshot_1_path, source_path)
+                assert_paths_identical(self, real_snapshot_1_path, source_path)
 
                 # Sleep for two seconds, so we are (hopefully) absolutely sure the time-based snapshot name generator
                 # will not generate identical names for all snapshots.
@@ -85,13 +85,13 @@ class BackupTest(TestCase):
                     f.flush()
                 result = backup(configuration)
                 self.assertTrue(result)
-                assert_path(self, os.path.join(
+                assert_paths_identical(self, os.path.join(
                     target_path, 'latest'), source_path)
                 # Ensure the changes made to the source did not affect the previous snapshots.
                 with self.assertRaises(AssertionError):
-                    assert_path(self, real_snapshot_1_path, source_path)
+                    assert_paths_identical(self, real_snapshot_1_path, source_path)
                 with self.assertRaises(AssertionError):
-                    assert_path(self, real_snapshot_2_path, source_path)
+                    assert_paths_identical(self, real_snapshot_2_path, source_path)
 
     @parameterized.expand([
         (DirectoryPath('sub/'),),
@@ -129,9 +129,9 @@ class BackupTest(TestCase):
                 real_snapshot_1_path = subprocess.check_output(['readlink', '-f', 'latest'], cwd=target_path).decode(
                     'utf-8').strip()
                 with self.assertRaises(AssertionError):
-                    assert_path(self, source_path, real_snapshot_1_path)
-                assert_path(self, os.path.join(source_path, str(path)),
-                            os.path.join(real_snapshot_1_path, str(path)))
+                    assert_paths_identical(self, source_path, real_snapshot_1_path)
+                assert_paths_identical(self, os.path.join(source_path, str(path)),
+                                       os.path.join(real_snapshot_1_path, str(path)))
 
                 # Sleep for two seconds, so we are (hopefully) absolutely sure the time-based snapshot name generator
                 # will not generate identical names for all snapshots.
@@ -142,12 +142,12 @@ class BackupTest(TestCase):
                 real_snapshot_2_path = subprocess.check_output(['readlink', '-f', 'latest'], cwd=target_path).decode(
                     'utf-8').strip()
                 with self.assertRaises(AssertionError):
-                    assert_path(self, source_path, real_snapshot_2_path)
-                assert_path(self, os.path.join(source_path, str(path)),
-                            os.path.join(real_snapshot_2_path, str(path)))
+                    assert_paths_identical(self, source_path, real_snapshot_2_path)
+                assert_paths_identical(self, os.path.join(source_path, str(path)),
+                                       os.path.join(real_snapshot_2_path, str(path)))
 
                 # Ensure the previous snapshot has not changed.
-                assert_path(self, os.path.join(real_snapshot_1_path, str(
+                assert_paths_identical(self, os.path.join(real_snapshot_1_path, str(
                     path)), os.path.join(source_path, str(path)))
 
     @parameterized.expand([
@@ -186,9 +186,9 @@ class BackupTest(TestCase):
                 real_snapshot_1_path = subprocess.check_output(['readlink', '-f', 'latest'], cwd=target_path).decode(
                     'utf-8').strip()
                 with self.assertRaises(AssertionError):
-                    assert_path(self, source_path, real_snapshot_1_path)
-                assert_path(self, os.path.join(source_path, str(path)),
-                            os.path.join(real_snapshot_1_path, str(path)))
+                    assert_paths_identical(self, source_path, real_snapshot_1_path)
+                assert_paths_identical(self, os.path.join(source_path, str(path)),
+                                       os.path.join(real_snapshot_1_path, str(path)))
 
                 # Sleep for two seconds, so we are (hopefully) absolutely sure the time-based snapshot name generator
                 # will not generate identical names for all snapshots.
@@ -199,14 +199,14 @@ class BackupTest(TestCase):
                 real_snapshot_2_path = subprocess.check_output(['readlink', '-f', 'latest'], cwd=target_path).decode(
                     'utf-8').strip()
                 with self.assertRaises(AssertionError):
-                    assert_path(self, source_path, real_snapshot_2_path)
-                assert_path(self, os.path.join(source_path, str(path)),
-                            os.path.join(real_snapshot_2_path, str(path)))
+                    assert_paths_identical(self, source_path, real_snapshot_2_path)
+                assert_paths_identical(self, os.path.join(source_path, str(path)),
+                                       os.path.join(real_snapshot_2_path, str(path)))
 
                 # Ensure the previous snapshot has not changed.
                 with self.assertRaises(AssertionError):
-                    assert_path(self, source_path, real_snapshot_1_path)
-                assert_path(self, os.path.join(real_snapshot_1_path, str(
+                    assert_paths_identical(self, source_path, real_snapshot_1_path)
+                assert_paths_identical(self, os.path.join(real_snapshot_1_path, str(
                     path)), os.path.join(source_path, str(path)))
 
     def test_backup_with_unavailable_source(self):
@@ -268,7 +268,7 @@ class RestoreTest(TestCase):
 
                 result = restore(configuration)
                 self.assertTrue(result)
-                assert_path(self, source_path, os.path.join(
+                assert_paths_identical(self, source_path, os.path.join(
                     target_path, 'latest'))
 
     @parameterized.expand([
@@ -305,10 +305,10 @@ class RestoreTest(TestCase):
                 result = restore(configuration, path)
                 self.assertTrue(result)
                 with self.assertRaises(AssertionError):
-                    assert_path(self, source_path, os.path.join(
+                    assert_paths_identical(self, source_path, os.path.join(
                         target_path, 'latest'))
-                assert_path(self, os.path.join(source_path, str(path)),
-                            os.path.join(target_path, 'latest', str(path)))
+                assert_paths_identical(self, os.path.join(source_path, str(path)),
+                                       os.path.join(target_path, 'latest', str(path)))
 
     @parameterized.expand([
         (FilePath('sub/some.file.in.subdirectory'),),
@@ -344,10 +344,10 @@ class RestoreTest(TestCase):
                 result = restore(configuration, path)
                 self.assertTrue(result)
                 with self.assertRaises(AssertionError):
-                    assert_path(self, source_path, os.path.join(
+                    assert_paths_identical(self, source_path, os.path.join(
                         target_path, 'latest'))
-                assert_path(self, os.path.join(source_path, str(path)),
-                            os.path.join(target_path, 'latest', str(path)))
+                assert_paths_identical(self, os.path.join(source_path, str(path)),
+                                       os.path.join(target_path, 'latest', str(path)))
 
     def test_restore_with_unavailable_source(self):
         # Create the source directory.
